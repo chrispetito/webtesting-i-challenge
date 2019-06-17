63b6fd7657390db2d5c877c0821b864722e8fe50
chrispetito <christopherpetito@gmail.com>
2019-06-17 11:04:37 -0700

    add repair test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("restores durability to 100", () => {
+      const item = { name: "item one", durability: 130, enhancement: 3 };
+      expect(enhancer.repair({ durability: 100}).durability).toBe(100);
+      expect(enhancer.repair({durability: -12}).durability).toBe(100);
+    });
+  });
+});
