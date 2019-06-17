2c7111b503c42c423f4501fd62f9b84fb03e6df5
chrispetito <christopherpetito@gmail.com>
2019-06-17 15:06:15 -0700

    add first succeed test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -4,8 +4,14 @@ describe("enhancer.js", () => {
   describe("repair()", () => {
     it("restores durability to 100", () => {
       const item = { name: "item one", durability: 130, enhancement: 3 };
-      expect(enhancer.repair({ durability: 100}).durability).toBe(100);
-      expect(enhancer.repair({durability: -12}).durability).toBe(100);
+      expect(enhancer.repair({ durability: 100 }).durability).toBe(100);
+      expect(enhancer.repair({ durability: -12 }).durability).toBe(100);
+    });
+  });
+  describe("succeed()", () => {
+    it("enhancement increases by one if it is less than 20", () => {
+        const item = { enhancement: 0 }
+      expect(enhancer.succeed(item).enhancement).toBe(1);
     });
   });
 });
